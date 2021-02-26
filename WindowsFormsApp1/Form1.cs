d56bd8491d0e20c87f5c569aacbfc5d168c945b2
Viral <Viral@DESKTOP-JU8HL42>
2021-02-26 14:04:32 +0500

    я сломал все

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
+           12312
         }
 
         private void button2_Click(object sender, EventArgs e)
